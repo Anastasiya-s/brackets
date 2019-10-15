7a1eae3cbf76dc0fccd97b89f625526252011001
Ana_stasya_S <anastasiya-s@live.de>
2019-10-15 14:10:24 +0300

    solution. Not works for the same open and close bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arr = str.split('');
+    let openBracketStack = [];
+    let result;
+    for ( let i = 0; i < arr.length; i++ ) {
+        bracketsConfig.forEach(bracket => {
+            if ( arr[i] === bracket[0] ) {
+                openBracketStack.push(arr[i]);
+            }
+            if ( arr[i] === bracket[1] && openBracketStack[openBracketStack.length - 1] === bracket[0]){
+                openBracketStack.pop();
+            }  
+            if ( arr[i] === bracket[1] && openBracketStack[openBracketStack.length - 1] !== bracket[0]){
+                return result = false;
+            }    
+        })
+    }
+    if (!openBracketStack.length) return result = true;
+    return result;
 }
