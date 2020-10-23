74e8b734ae9735f7128eadc383eb563a0da50b66
Ana_stasya_S <anastasiya-s@live.de>
2020-10-23 13:57:05 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-    const arr = str.split('');
-    let openBracketStack = [];
-    let result;
-    for ( let i = 0; i < arr.length; i++ ) {
-        bracketsConfig.forEach(bracket => {
-            if ( arr[i] === bracket[0] ) {
-                openBracketStack.push(arr[i]);
+    let result = true;
+    const tempString = str.split('');
+    const stack = [];
+
+    for ( i = 0; i < tempString.length; i++) {
+        for (j = 0; j < bracketsConfig.length; j++) {
+            const currentItem = tempString[i]
+            const openBracket = bracketsConfig[j][0]
+            const closeBracket = bracketsConfig[j][1]
+            if (currentItem !== openBracket && currentItem !== closeBracket) continue
+            if (currentItem === openBracket && openBracket === closeBracket) {
+                if (stack[stack.length - 1] !== openBracket) {
+                    stack.push(currentItem)
+                    break
+                } else if (stack[stack.length - 1] === openBracket) {
+                    stack.pop()
+                    continue    
+                } else return result = false
             }
-            if ( arr[i] === bracket[1] && openBracketStack[openBracketStack.length - 1] === bracket[0]){
-                openBracketStack.pop();
-            }  
-            if ( arr[i] === bracket[1] && openBracketStack[openBracketStack.length - 1] !== bracket[0]){
-                return result = false;
-            }    
-        })
+            if (currentItem === openBracket) {
+                stack.push(currentItem)
+                continue
+            } else if (currentItem === closeBracket) {
+                if(stack.length !== 0 && stack[stack.length - 1] === openBracket) {
+                    stack.pop()
+                    continue
+                } else {                    
+                    return result = false;                    
+                }
+            }
+            if (!result) break
+        }
     }
-    if (!openBracketStack.length) return result = true;
-    return result;
+    if (stack.length > 0 ) result = false
+   return result;
 }
